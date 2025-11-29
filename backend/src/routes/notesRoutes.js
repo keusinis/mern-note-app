ccfd6a92a6121da22a0af3ce6fddc943120f0231
keusinis <220072052+keusinis@users.noreply.github.com>
2025-11-29 22:11:09 +0200

    Fix formatting

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import { createNote, deleteNote, getAllNotes, updateNote, getNote } from '../controllers/notesController.js';
+import express from "express";
+import {
+  createNote,
+  deleteNote,
+  getAllNotes,
+  updateNote,
+  getNote,
+} from "../controllers/notesController.js";
 
 const router = express.Router();
 
-router.get('/', getAllNotes);
-router.get('/:id', getNote);
-router.post('/', createNote);
-router.put('/:id', updateNote); 
-router.delete('/:id', deleteNote); 
+router.get("/", getAllNotes);
+router.get("/:id", getNote);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router;
